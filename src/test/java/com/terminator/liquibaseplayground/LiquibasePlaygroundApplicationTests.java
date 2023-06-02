03efda282be6102f159349cacc71195101e47fcf
berna39 <kalemajoseph39@gmail.com>
2023-06-02 15:07:49 +0200

    :spakles: initial examle

---
+++ src/test/java/com/terminator/liquibaseplayground/LiquibasePlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terminator.liquibaseplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibasePlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
